277e52d80da55a7e378e1816063a19e96a819e33
Ian Mihura <ian@oneragtime.com>
2023-12-09 14:26:05 +0100

    day 9 part 1

---
+++ day_9/day_9.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	data, _ := os.ReadFile("./full_input.txt")
+	fdata := strings.Split(string(data), "\n")
+	answer := run(fdata)
+	fmt.Println(answer)
+
+}
+
+func run(fdata []string) int {
+	var trailing int
+	for _, v := range fdata {
+		var current_seq [][]int
+		current_line := convert_line(strings.Split(v, " "))
+		current_seq = append(current_seq, current_line)
+		var step int
+		for {
+			differences := get_differences(current_seq[step])
+			current_seq = append(current_seq, differences)
+			if is_final(differences) {
+				break
+			} else {
+				step++
+			}
+		}
+		trailing += get_next_number(current_seq)
+	}
+	return trailing
+}
+
+func get_next_number(current_seq [][]int) int {
+	var next_numbers []int
+	for i := len(current_seq) - 1; i >= 0; i-- {
+		next_numbers = append(next_numbers, current_seq[i][len(current_seq[i])-1])
+	}
+	fmt.Println(next_numbers)
+	return add_reduce(next_numbers)
+}
+
+func add_reduce(list []int) int {
+	var value int
+	for i := range list {
+		value += list[i]
+	}
+	return value
+}
+
+func is_final(line []int) bool {
+	for k := range line {
+		if line[k] != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func get_differences(line []int) []int {
+	var differences []int
+	for k := range line {
+		if k != 0 {
+			differences = append(differences, line[k]-line[k-1])
+		}
+	}
+	return differences
+}
+
+func convert_line(line []string) []int {
+	var fline []int
+	for k := range line {
+		line_k, _ := strconv.Atoi(line[k])
+		fline = append(fline, line_k)
+	}
+	return fline
+}
