5f8554f17bfa39bbdf3d70523a3c60fc1e6e8180
Ian Mihura <ian@oneragtime.com>
2023-12-11 22:41:53 +0100

    day 6

--- day_6/day_6.go
+++ day_6/day_6.go
@@ -3,16 +3,36 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	data, _ := os.ReadFile("./sample_input.txt")
-	fdata := strings.Split(string(data), "\n")
-	answer := run(fdata)
-	fmt.Println(answer[0])
+	data, _ := os.ReadFile("./full_input.txt")
+	time_data := strings.Fields(strings.Split(string(data), "\n")[0])[1:]
+	dist_data := strings.Fields(strings.Split(string(data), "\n")[1])[1:]
+	answer := run(time_data, dist_data)
+	fmt.Println(answer)
 }
 
-func run(fdata []string) []string {
-	return fdata
+func run(time_data, dist_data []string) int {
+	var race_duration, record_dist string
+	for i := 0; i < len(time_data); i++ {
+		race_duration += time_data[i]
+		record_dist += dist_data[i]
+	}
+	int_race_duration, _ := strconv.Atoi(race_duration)
+	int_record_dist, _ := strconv.Atoi(record_dist)
+	return get_best_for_race(int_race_duration, int_record_dist)
+}
+
+func get_best_for_race(race_duration, record_dist int) int {
+	best := 0
+
+	for i := 0; i < race_duration; i++ {
+		if i*(race_duration-i) > record_dist {
+			best++
+		}
+	}
+	return best
 }
